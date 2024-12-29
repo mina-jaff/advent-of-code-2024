60235d09aff1bf506ac6236ffd1a9135b957717b
Mina <144630931+mina-jaff@users.noreply.github.com>
2024-12-29 15:06:24 +0100

    updated solution for Day 2

--- day2/solution.js
+++ day2/solution.js
@@ -40,4 +40,4 @@ const findSafeReports = (unusualData) => {
 };
 
 
-console.log(findSafeReports([[1, 3, 5], [7, 2, 5]]));
+console.log(findSafeReports([[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]));
